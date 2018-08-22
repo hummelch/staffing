ab93bc45c4e7df5ba7d2839d33e114e39d2e3195
janrembold <janrembold@gmx.net>
2018-08-22 12:49:28 +0200

    added description list component

---
+++ src/components/descriptionList/DescriptionList.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './descriptionList.css';
+
+class DescriptionList extends Component {
+  render() {
+    return (
+      <dl className="descriptionList">
+        {this.props.items.map(item => (
+          <React.Fragment>
+            <dt className="descriptionList__title">{item.title}</dt>
+            <dd className="descriptionList__description">{item.description}</dd>
+          </React.Fragment>
+        ))}
+      </dl>
+    );
+  }
+}
+
+DescriptionList.propTypes = {
+  items: PropTypes.array
+};
+
+export default DescriptionList;
