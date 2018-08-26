5ea1b81cdddaa6b955bb98299ec6ee5b18968dfb
janrembold <janrembold@gmx.net>
2018-08-26 17:33:14 +0200

    replace for..of with array forEach

--- src/staffing/prepareProjectsData.js
+++ src/staffing/prepareProjectsData.js
@@ -5,20 +5,21 @@ export const prepareProjectsData = (stateProjects) => {
     closed: []
   };
 
-  for(let stateProject of stateProjects) {
-    const project = Object.assign({}, stateProject);
-    project.staffed_days = project.staffings.reduce((acc, val) => acc + val.days, 0);
+  stateProjects.forEach(stateProject => {
+    const project = { ...stateProject };
 
-    if(project.closed) {
+    if (project.closed) {
       result.closed.push(project);
     } else {
-      if(project.staffed_days >= project.estimation_days) {
+      const staffedDays = project.staffings.reduce((acc, val) => acc + val.days, 0);
+
+      if (staffedDays >= project.estimation_days) {
         result.staffed.push(project)
       } else {
         result.open.push(project)
       }
     }
-  }
+  });
 
   return result;
 };
