ba04abe305badcb9bb1527b78255381105bf3c03
Jan Rembold <jan.rembold@virtual-identity.com>
2018-08-28 17:39:02 +0200

    initial draft - TODO move to tooltip component

--- src/components/workload/WorkloadRow.jsx
+++ src/components/workload/WorkloadRow.jsx
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const defaultState = {
+  active: null
+};
+
 class WorkloadRow extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleTooltipClick = this.handleTooltipClick.bind(this);
+    this.renderTooltip = this.renderTooltip.bind(this);
+
+    this.state = defaultState;
+  }
 
   getClassNameByWorkload(daysLeft) {
     if (daysLeft < 0) {
@@ -13,6 +25,37 @@ class WorkloadRow extends Component {
     }
 
     return 'workload__cell--ok';
+  }
+
+  handleTooltipClick(e) {
+    e.preventDefault();
+
+    console.log('click');
+    const id = parseInt(e.target.getAttribute('data-user-id'), 10);
+    const week = e.target.getAttribute('data-week');
+
+    if(this.state.active && this.state.active.id === id && this.state.active.week === week) {
+      this.setState(defaultState);
+    } else {
+      this.setState({
+        active: {
+          id: id,
+          week: week
+        }
+      });
+    }
+  }
+
+  renderTooltip(id, week) {
+    if(this.state.active) {
+      console.log('render', id, week, this.state.active.id, this.state.active.week);
+    }
+
+    if(!this.state.active || this.state.active.id !== id || this.state.active.week !== week) {
+      return '';
+    }
+
+    return <div className="workload__tooltip">Tooltip for {id} - {week}</div>;
   }
 
   render() {
@@ -29,8 +72,9 @@ class WorkloadRow extends Component {
         <td className="workload__cell workload__cell--name">{user.name}</td>
 
         {Object.keys(weeks).map(week => (
-          <td key={`${user.id}-${week}`} className={`workload__cell ${this.getClassNameByWorkload(weeks[week].days_left)}`}>
+          <td onClick={this.handleTooltipClick} data-user-id={user.id} data-week={week} key={`${user.id}-${week}`} className={`workload__cell ${this.getClassNameByWorkload(weeks[week].days_left)}`}>
             {weeks[week].days_left}
+            {this.renderTooltip(user.id, week)}
           </td>
         ))}
       </tr>
