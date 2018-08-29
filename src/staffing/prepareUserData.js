516b33af12daf9a1867ede3a5ec969863dc771f8
janrembold <janrembold@gmx.net>
2018-08-29 21:29:49 +0200

    add unavailable state in workload table

--- src/staffing/prepareUserData.js
+++ src/staffing/prepareUserData.js
@@ -3,10 +3,18 @@ export const calculateWorkloadForUser = (stateUser, stateProjects) => {
 
   // count up all default week days for all 52 weeks per year
   for (let week = 1; week <= 52; week++) {
-    const daysLeft = stateUser.diff_days.hasOwnProperty(week) ? stateUser.diff_days[week] : stateUser.days_per_week;
+
+    let isNotAvailable = stateUser.days_per_week === 0;
+    const weekHasSpecialValue = stateUser.diff_days.hasOwnProperty(week);
+    const daysLeft = weekHasSpecialValue ? stateUser.diff_days[week] : stateUser.days_per_week;
+
+    if (weekHasSpecialValue && stateUser.diff_days[week] === 0) {
+      isNotAvailable = true;
+    }
 
     user.calculated_weeks[week] = {
-      days_left: daysLeft
+      days_left: daysLeft,
+      is_not_available: isNotAvailable
     };
   }
 
@@ -22,8 +30,9 @@ export const calculateWorkloadForUser = (stateUser, stateProjects) => {
 
 const subtractStaffedWorkingDays = (user, project) => {
   for (let staffing of project.staffings) {
-    if(user.id === staffing.user_id) {
+    if (user.id === staffing.user_id) {
       user.calculated_weeks[staffing.week].days_left -= staffing.days;
     }
   };
 };
+
