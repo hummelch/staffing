0bbe510e1484a401e01c7e2660b966332e5df558
janrembold <janrembold@gmx.net>
2018-09-05 22:01:01 +0200

    add date helper to convert week string into object with year and week as properties

--- src/staffing/dateHelper.js
+++ src/staffing/dateHelper.js
@@ -1,7 +1,12 @@
 export const getWeekNumber = (today = new Date()) => {
-  const d = new Date(Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()));
-  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    const d = new Date(Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
 
-  const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-  return Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    return Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+};
+
+export const getYearAndWeekFromString = (weekString) => {
+    const date = weekString.split('-');
+    return { year: date[0], week: date[1] };
 };
