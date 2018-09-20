8317efa8912059848d81cd4ee8cd312abbd4084c
Jan Rembold <jan.rembold@virtual-identity.com>
2018-09-20 11:41:03 +0200

    minor var name refactoring

--- src/staffing/prepareUserData.js
+++ src/staffing/prepareUserData.js
@@ -1,30 +1,30 @@
-export const calculateWorkloadForUser = (stateUser, stateProjects, stateUserCustomDays) => {
-  const user = Object.assign({}, stateUser, { calculated_weeks: {} });
+export const calculateWorkloadForUser = (user, projects, userCustomDays) => {
+  const updatedUser = Object.assign({}, user, { calculated_weeks: {} });
 
   // count up all default week days for all 52 weeks per year
   for (let week = 1; week <= 52; week++) {
-    user.calculated_weeks[week] = {
-      days_left: stateUser.days_per_week,
-      is_not_available: stateUser.days_per_week === 0
+    updatedUser.calculated_weeks[week] = {
+      days_left: user.days_per_week,
+      is_not_available: user.days_per_week === 0
     };
   }
 
   // check custom days
-  stateUserCustomDays.forEach(custom => {
-    if(custom.user_id === stateUser.id) {
-      user.calculated_weeks[custom.week].days_left = custom.days;
-      user.calculated_weeks[custom.week].is_not_available = custom.days === 0;
+  userCustomDays.forEach(custom => {
+    if(custom.user_id === user.id) {
+      updatedUser.calculated_weeks[custom.week].days_left = custom.days;
+      updatedUser.calculated_weeks[custom.week].is_not_available = custom.days === 0;
     }
   });
 
   // subtract all staffed projects
-  for (let project of stateProjects) {
+  for (const project of projects) {
     if (!project.closed) {
-      subtractStaffedWorkingDays(user, project);
+      subtractStaffedWorkingDays(updatedUser, project);
     }
   }
 
-  return user;
+  return updatedUser;
 };
 
 const subtractStaffedWorkingDays = (user, project) => {
