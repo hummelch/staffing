bbcaa512d25ad179a0d81a84301fb28569e1d303
Jan Rembold <jan.rembold@virtual-identity.com>
2018-10-05 11:03:13 +0200

    refactor to hash router

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
 
 import {config} from '../../config';
 import Dashboard from '../dashboard/Dashboard';
