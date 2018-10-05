9762dffd1fc8af413b2548d825ccd45b0f7861c1
Jan Rembold <jan.rembold@virtual-identity.com>
2018-10-05 21:44:15 +0200

    add styling for loading spinner info

--- src/components/loadingSpinner/LoadingSpinner.jsx
+++ src/components/loadingSpinner/LoadingSpinner.jsx
@@ -9,7 +9,7 @@ class LoadingSpinner extends Component {
         <svg className="loadingSpinner__spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
           <circle className="loadingSpinner__path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
         </svg>
-        <small>The app is loading or you were idle for more than {config.idleReloadSeconds} seconds.</small>
+        <small className="loadingSpinner_information">The app is loading or you were idle for more than {config.idleReloadSeconds} seconds.</small>
       </div>
     );
   }
