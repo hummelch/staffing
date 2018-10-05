4a8d748aaa33ae2b265b578df757d1163db24c61
Jan Rembold <jan.rembold@virtual-identity.com>
2018-10-05 21:42:11 +0200

    add idle info to loading spinner

--- src/components/loadingSpinner/LoadingSpinner.jsx
+++ src/components/loadingSpinner/LoadingSpinner.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './loadingSpinner.css';
+import {config} from "../../config";
 
 class LoadingSpinner extends Component {
   render() {
@@ -8,6 +9,7 @@ class LoadingSpinner extends Component {
         <svg className="loadingSpinner__spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
           <circle className="loadingSpinner__path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
         </svg>
+        <small>The app is loading or you were idle for more than {config.idleReloadSeconds} seconds.</small>
       </div>
     );
   }
