2969ce46b116eed742f82730d608720c15a82e28
Jan Rembold <jan.rembold@virtual-identity.com>
2018-10-05 11:42:51 +0200

    add host form to loading screen if error happend

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 import Navigation from './components/navigation/Navigation';
 import LoadingSpinner from './components/loadingSpinner/LoadingSpinner';
+import HostForm from './components/host/HostForm';
 
 const mapStateToProps = state => {
-  return { isLoading: state.isLoading };
+  return {
+    isLoading: state.isLoading,
+    errorMessage: state.errorMessage
+  };
 };
 
 class App extends Component {
   render() {
     if (this.props.isLoading) {
       return (
-        <LoadingSpinner />
+        <div>
+          {this.props.errorMessage ? <HostForm /> : ''}
+          <LoadingSpinner/>
+        </div>
       );
     }
 
     return (
       <div className="App">
-        <Navigation />
+        <Navigation/>
       </div>
     );
   }
