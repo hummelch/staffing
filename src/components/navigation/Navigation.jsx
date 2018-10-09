378dca82bdee1c783ae9c83f82585f2f78747de4
Jan Rembold <jan.rembold@virtual-identity.com>
2018-10-09 09:08:58 +0200

    rename db host nav item to Config

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -5,7 +5,7 @@ import {config} from '../../config';
 import Dashboard from '../dashboard/Dashboard';
 import ProjectForm from '../projects/ProjectForm';
 import UserForm from '../user/UserForm';
-import HostForm from '../host/ConfigForm';
+import ConfigForm from '../host/ConfigForm';
 
 import './navigation.css';
 
@@ -19,14 +19,14 @@ class Navigation extends Component {
             <li><NavLink exact className="navigation__link" activeClassName="navigation__link--active" to="/">Dashboard</NavLink></li>
             <li><NavLink exact className="navigation__link" activeClassName="navigation__link--active" to="/project">New Project</NavLink></li>
             <li><NavLink exact className="navigation__link" activeClassName="navigation__link--active" to="/user">New User</NavLink></li>
-            <li><NavLink exact className="navigation__link" activeClassName="navigation__link--active" to="/host">DB Host</NavLink></li>
+            <li><NavLink exact className="navigation__link" activeClassName="navigation__link--active" to="/config">Config</NavLink></li>
           </ul>
 
           <main className="content">
             <Route exact path="/" component={Dashboard} />
             <Route path="/project" component={ProjectForm} />
             <Route path="/user" component={UserForm} />
-            <Route path="/host" component={HostForm} />
+            <Route path="/config" component={ConfigForm} />
           </main>
 
         </div>
