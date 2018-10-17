f38c64ddd7d3859bbd4fd09055086a0ba7029c87
janrembold <janrembold@gmx.net>
2018-10-17 20:46:13 +0200

    extend dashboard with team filter

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
 import Workload from '../workload/Workload';
 import Projects from '../projects/Projects';
+import {connect} from "react-redux";
+import './dashboard.css';
+
+const mapStateToProps = state => {
+  const teams = [];
+
+  state.data.users.forEach(element => {
+    if(element.team && teams.indexOf(element.team) === -1) {
+      teams.push(element.team);
+    }
+  });
+
+  teams.sort();
+  return {teams};
+};
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      team: ''
+    };
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({
+      team: e.target.value
+    });
+  }
+
   render() {
+    const teamOptions = this.props.teams.map((team, idx) => <option key={idx} value={team}>{team}</option>);
+    teamOptions.unshift(<option key="-1" value="">All</option>);
+
     return (
       <div className="dashboard">
 
         <div className="grid-x grid-padding-x">
           <div className="cell">
-            <h4>Team Overview</h4>
-            <Workload />
+            <h4 className="dashboard__workloadHeadline">Team Overview</h4>
+            <select onChange={this.handleChange} className="dashboard__teamSelect">
+              {teamOptions}
+            </select>
+            <Workload team={this.state.team} />
           </div>
         </div>
 
@@ -29,4 +66,4 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+export default connect(mapStateToProps)(Dashboard);
